ed3afdc90919bde9f21ad68123a30de5344805be
cgyli <cgli@sfu.ca>
2017-06-07 16:21:34 -0700

    First Intro to Arduino Uno
    Programming pin 13 (LED) on and off.

---
+++ sketch_jun07a.ino
@@ -0,0 +1,14 @@
+int ledPin = 13;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(ledPin, OUTPUT);  //takes variable of input or output
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(ledPin, HIGH); //initial off
+  delay(2000);
+  digitalWrite(ledPin, LOW);
+  delay(2000);
+}
